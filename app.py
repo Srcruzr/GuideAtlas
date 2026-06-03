5dc9c54f3136407e104c56f1b6f5594b85b4301b
Srcruzr <cruzsoftnyx@gmail.com>
2026-06-03 18:11:07 -0500

    google verification file

--- app.py
+++ app.py
@@ -26,5 +26,9 @@ def cofres_secretos():
 def cofres_secretos_ashen_ruins():
     return render_template("cofres-secretos-ashen-ruins.html")
 
+@app.route("/googlee6a931a6b04cb4d2")
+def googlee6a931a6b04cb4d2():
+    return render_template("googlee6a931a6b04cb4d2.html")
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000, debug=True)
